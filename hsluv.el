bc6e27d25b62f5a2f79836a32e8de6125f4d1564
gvermeiren <geert.vermeiren@persgroep.be>
2018-11-27 13:06:50 +0100

    Added package dependencies

--- hsluv.el
+++ hsluv.el
@@ -6,6 +6,7 @@
 ;; Author: Geert Vermeiren
 ;; Keywords: color hsluv
 ;; URL: https://github.com/hsluv/hsluv-emacs
+;; Package-Requires: ((seq "2.20"))
 
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy of
 ;; this software and associated documentation files (the "Software"), to deal in
